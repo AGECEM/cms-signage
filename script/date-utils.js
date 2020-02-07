18111ca1409b2e6b0fcd65c2b7d62742ef9cd83d
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2020-02-07 00:45:15 -0500

    Affichage de la date, css déplacé dans un fichier externe, fonctions js génériques déplacés dans un fichier externe, arrière plan de la bar est maintenant un gradient css plutôt qu'une image.

---
+++ script/date-utils.js
@@ -0,0 +1,69 @@
+function setDateTime() {
+    document.getElementById("time").innerHTML = currentTime();
+    document.getElementById("date").innerHTML = currentDate();
+}
+
+function getDayOfWeek(day) {
+    switch (day) {
+        case 0:
+            return "Dim";
+        case 1:
+            return "Lun";
+        case 2:
+            return "Mar";
+        case 3:
+            return "Mer";
+        case 4:
+            return "Jeu";
+        case 5:
+            return "Ven";
+        case 6:
+            return "Sam";
+    }
+}
+
+function getMonth(month) {
+    switch (month) {
+        case 0:
+            return "Jan";
+        case 1:
+            return "Fev";
+        case 2:
+            return "Mar";
+        case 3:
+            return "Avr";
+        case 4:
+            return "Mai";
+        case 5:
+            return "Juin";
+        case 6:
+            return "Juil";
+        case 7:
+            return "Août";
+        case 8:
+            return "Sep";
+        case 9:
+            return "Oct";
+        case 10:
+            return "Nov";
+        case 11:
+            return "Déc";
+    }
+}
+
+function currentDate() {
+    const d = new Date();
+    const day = getDayOfWeek(d.getDay());
+    const date = d.getDate();
+    const month = getMonth(d.getDay());
+
+    return day + " " + date + " " + month;
+}
+
+function currentTime() {
+    const date = new Date();
+    const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+
+    return hours + ":" + minutes;
+}
