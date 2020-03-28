20e8d3a7979e4e8789c4cc6277d289281a3c6443
Victor Lacasse-Beaudoin <vlbeaudoin@users.noreply.github.com>
2020-03-28 19:22:21 -0400

    Obtention des images sans GLOB_BRACE
    Problèmes de compatibilité avec image docker

--- getImages.php
+++ getImages.php
@@ -1,5 +1,23 @@
 <?php
 $dir = "contenu";
-$files = glob($dir . '/*.{jpg,png,jpeg,gif,pdf}', GLOB_BRACE);
 
+for ($index = 0; $index < 4; $index++){
+        switch ($index){
+        case 0:
+                $files = glob("$dir/*.png");
+                break;
+        case 1:
+                $files += glob("$dir/*.jpg");
+                break;
+        case 2:
+                $files += glob("$dir/*.jpeg");
+                break;
+        case 3:
+                $files += glob("$dir/*.gif");
+                break;
+        default:
+                echo "This shouldn't happen";
+                break;
+        }
+}
 echo json_encode($files);
